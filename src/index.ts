1e481be73a64a5bd47fc54d2405825bb96e3e812
Max Koverdyaev <shadeglare@gmail.com>
2017-10-22 14:55:18 +0300

    fixed

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as Iters from "./iters";
 import { Enumerable, Grouping } from "./enumerable";
-import * as Dictionary from "./dictionary";
+import Dictionary from "./dictionary";
 
 export { Iters, Enumerable, Grouping, Dictionary };
